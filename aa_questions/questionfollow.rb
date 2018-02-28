00a8a885d4c646a74d2a59ad8aa3a9a82b83a892
alexandra <alexandra.savramis@gmail.com>
2018-02-27 16:56:09 -0800

    Create question db classes

---
+++ aa_questions/questionfollow.rb
@@ -0,0 +1,56 @@
+require_relative 'questions_database.rb'
+
+class QuestionFollow
+  attr_accessor :user_id, :question_id
+  attr_reader :id
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM question_follows")
+    data.map { |datum| QuestionFollow.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    follow = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        question_follows
+      WHERE
+        id = ?
+    SQL
+
+    # p question
+    return nil if follow.empty?
+
+    QuestionFollow.new(follow.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @user_id = options['user_id']
+    @question_id = options['question_id']
+  end
+
+  def create
+    raise "#{self} already in database" if @id
+    QuestionsDatabase.instance.execute(<<-SQL, @user_id, @question_id)
+      INSERT INTO
+        question_follows (user_id, question_id)
+      VALUES
+        (?, ?)
+    SQL
+    @id = QuestionsDatabase.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    QuestionsDatabase.instance.execute(<<-SQL, @user_id, @question_id, @id)
+      UPDATE
+        question_follows
+      SET
+        user_id = ?, question_id = ?
+      WHERE
+        id = ?
+    SQL
+  end
+end
